8706ad2673c4171adf5e6a9404068fd2fe5efc5d
Allllore <2050791391@qq.com>
2024-04-09 12:20:59 +0800

    添加跨域处理

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 from flask import Flask, request, jsonify, render_template
 import logging
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
